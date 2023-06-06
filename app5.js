de7d01d32b7a800b8044daf0dfea434bdd73c26d
aron <114922035+aronpaco@users.noreply.github.com>
2023-06-06 13:24:55 +0300

    Initial commit

---
+++ app5.js
@@ -0,0 +1,11 @@
+
+const EventEmitter = require('events');
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+logger.on('messageLogged', (arg) => {
+    console.log('Listener aclled', arg);
+});
+
+logger.log('message');
